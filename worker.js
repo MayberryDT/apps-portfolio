86e2ad2ec7713d68bff6c168781930262559b17a
Tyler Mayberry <mayberrydt@gmail.com>
2026-07-21 00:34:43 -0500

    Block hidden paths from Worker assets

--- worker.js
+++ worker.js
@@ -1,5 +1,10 @@
 export default {
   fetch(request, env) {
+    const path = new URL(request.url).pathname;
+    if (path.split("/").some((segment) => segment.startsWith("."))) {
+      return new Response("Not found", { status: 404 });
+    }
+
     return env.ASSETS.fetch(request);
   }
 };
